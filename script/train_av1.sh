5593537e74ae1bb4a65d377ab3fd2e40a00df7c0
kibnapper <1259478761@qq.com>
2023-07-11 00:13:11 +0800

    增加了一个训练脚本

---
+++ script/train_av1.sh
@@ -0,0 +1,16 @@
+python src/run.py \
+    --core_num 36 --distributed_training 2 \
+    --use_map \
+    --use_centerline \
+    --argoverse \
+    --future_frame_num 60 \
+    --do_train \
+    --data_dir /home/DATA/yipin/dataset/Argoverse/motion_forecasting/train/data \
+    --output_dir /home/yipin/program/nas/nasTNT/DenseTNT/ckpt/av1/densetnt/1 \
+    --hidden_size 128 \
+    --train_batch_size 64 \
+    --reuse_temp_file \
+    --other_params \
+    semantic_lane direction l1_loss \
+    goals_2D enhance_global_graph subdivide goal_scoring laneGCN point_sub_graph \
+    lane_scoring complete_traj complete_traj-3 \
